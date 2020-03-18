acb78a37f6d14d742c3068f582d13d498512595d
Romit Karmakar <romitkarmakar@gmail.com>
2020-03-19 00:55:51 +0530

    Added travis ci

--- test/main.dart
+++ test/main.dart
@@ -1,9 +1,11 @@
+import 'package:dartyaml/main.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('String.split() splits the string on the delimiter', () {
-    var string = 'foo,bar,baz';
-    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  test('String.split() splits the string on the delimiter', () async {
+    var string = 'name: dartyaml';
+    TreeNode temp = await readFromString(string);
+    expect(temp.value, equals('root'));
   });
 
   test('String.trim() removes surrounding whitespace', () {
